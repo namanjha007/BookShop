bf432098c5c7573886ec4aaad2635a546164a62d
Naman Kumar Jha <naman.jha.10@gmail.com>
2020-04-11 13:12:44 +0530

    Added Image Slidder using View Flipper 11.04.2020..13:12

--- app/src/main/java/com/example/codemail/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/codemail/ui/home/HomeFragment.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.ViewFlipper;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
@@ -15,15 +17,32 @@ import androidx.lifecycle.ViewModelProviders;
 import com.example.codemail.R;
 
 public class HomeFragment extends Fragment {
-
-    private HomeViewModel homeViewModel;
-
+    ViewFlipper vFlipper;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+        int images[] = {R.drawable.book, R.drawable.book1, R.drawable.book2, R.drawable.book3};
+        vFlipper = (ViewFlipper)root.findViewById(R.id.vFlipper);
+
+        for(int i=0;i<images.length;i++)
+        {
+            flipperImages(images[i]);
+        }
+
+
 
         return root;
+    }
+    public void flipperImages(int image)
+    {
+        ImageView imageView = new ImageView(getContext());
+        imageView.setBackgroundResource(image);
+
+        vFlipper.addView(imageView);
+        vFlipper.setFlipInterval(2500);
+        vFlipper.setAutoStart(true);
+
+        vFlipper.setInAnimation(getContext(), android.R.anim.slide_in_left);
+        vFlipper.setOutAnimation(getContext(), android.R.anim.slide_out_right);
     }
 }
