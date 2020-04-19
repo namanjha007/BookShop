8c7e6881b0e87fdd6afbcab9cfa9ed67ab470a8d
Naman Kumar Jha <naman.jha.10@gmail.com>
2020-04-19 10:45:08 +0530

    Added Buttons and Completed Home Fragment 12.04.2020-01:27

--- app/src/main/java/com/example/codemail/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/codemail/ui/home/HomeFragment.java
@@ -6,110 +6,170 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.ViewFlipper;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.codemail.R;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 
 public class HomeFragment extends Fragment {
 
-
-    RecyclerView recyclerView, recyclerView2;
-
-    ArrayList<MainModel> mainModels;
-    MainAdapter mainAdapter;
-
-    ArrayList<MainModel2> mainModel2s;
-    MainAdapter2 mainAdapter2;
+    RecyclerView recyclerView, recyclerView1;
+    DatabaseReference databaseReference, databaseReference1;
+    FirebaseRecyclerOptions<LiveBooks> options;
+    FirebaseRecyclerAdapter<LiveBooks,CategoryViewHolder> adapter;
+    FirebaseRecyclerOptions<LiveBooks1> options1;
+    FirebaseRecyclerAdapter<LiveBooks1,CategoryViewHolder1> adapter1;
 
     ViewFlipper vFlipper , vFlipper2;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        recyclerView = (RecyclerView)root.findViewById(R.id.recycler_view2);
 
-        Integer[] schoollogo = {R.drawable.loyola_logo, R.drawable.hilltop_logo, R.drawable.lfs_logo,
-                R.drawable.vbcv_logo, R.drawable.dav_logo, R.drawable.dbms_logo, R.drawable.ghs_logo,
-                R.drawable.rv_logo, R.drawable.vvs_logo, R.drawable.ksms_logo};
-
-        String[] schoolname = {"Loyola","HTS","LFS","VBCV","DAV","DBMS","GHS","RV","Valley View","KSMS"};
-
-        mainModels = new ArrayList<>();
-        for(int i=0;i<schoollogo.length;i++)
-        {
-            MainModel model = new MainModel(schoollogo[i], schoolname[i]);
-            mainModels.add(model);
-        }
-
-        LinearLayoutManager layoutManager = new LinearLayoutManager(
-                getContext(), LinearLayoutManager.HORIZONTAL,false
-        );
-        recyclerView.setLayoutManager(layoutManager);
-        recyclerView.setItemAnimator(new DefaultItemAnimator());
-
-        mainAdapter = new MainAdapter(getContext(),mainModels);
-        recyclerView.setAdapter(mainAdapter);
-
-
-
-
-        recyclerView2 = (RecyclerView)root.findViewById(R.id.recycler_view1);
-
-        Integer[] schoollogo2 = {R.drawable.ksms_logo, R.drawable.dav_logo, R.drawable.loyola_logo,
-                R.drawable.dbms_logo, R.drawable.rv_logo, R.drawable.hilltop_logo, R.drawable.ghs_logo,
-                R.drawable.lfs_logo, R.drawable.vvs_logo, R.drawable.vbcv_logo};
-
-        String[] schoolname2 = {"KSMS","DAV","Loyola","DBMS","RV","HTS","GHS","LFS","Valley View","VBCV"};
-
-        mainModel2s = new ArrayList<>();
-        for(int j=0;j<schoollogo2.length;j++)
-        {
-            MainModel2 model2 = new MainModel2(schoollogo2[j], schoolname2[j]);
-            mainModel2s.add(model2);
-        }
-
-        LinearLayoutManager layoutManager2 = new LinearLayoutManager(
-                getContext(), LinearLayoutManager.HORIZONTAL,false
-        );
-        recyclerView2.setLayoutManager(layoutManager2);
-        recyclerView2.setItemAnimator(new DefaultItemAnimator());
-
-        mainAdapter2 = new MainAdapter2(getContext(),mainModel2s);
-        recyclerView2.setAdapter(mainAdapter2);
+        Integer[] a = {R.drawable.book3, R.drawable.book2, R.drawable.book1, R.drawable.book};
 
         vFlipper = (ViewFlipper)root.findViewById(R.id.vFlipper);
         vFlipper2 = (ViewFlipper)root.findViewById(R.id.vFlipper2);
-
-//        String[] a = {"https://firebasestorage.googleapis.com/v0/b/codemail-88765.appspot.com/o/book3.png?alt=media&token=2b1a85dc-48a9-4857-bca5-c81b3f831934",
-//                "https://firebasestorage.googleapis.com/v0/b/codemail-88765.appspot.com/o/book2.jpg?alt=media&token=474eeea6-6472-4432-b044-370dcc0d68f4",
-//        "https://firebasestorage.googleapis.com/v0/b/codemail-88765.appspot.com/o/book1.jpg?alt=media&token=16a8bbc6-24f8-4919-a479-19d82c6d4557",
-//        "https://firebasestorage.googleapis.com/v0/b/codemail-88765.appspot.com/o/book.jpg?alt=media&token=7aab5efb-1d94-4629-b17d-7804907a3552"};
-
-        Integer[] a = {R.drawable.book3, R.drawable.book2, R.drawable.book1, R.drawable.book};
 
         for(int i=0;i<4;i++)
         {
             flipperImages(a[i]);
         }
 
+        recyclerView = (RecyclerView)root.findViewById(R.id.recycler_view2);
+        recyclerView1 = (RecyclerView)root.findViewById(R.id.recycler_view1);
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("Live_School");
+        options = new FirebaseRecyclerOptions.Builder<LiveBooks>()
+                .setQuery(databaseReference,LiveBooks.class).build();
+        adapter = new FirebaseRecyclerAdapter<LiveBooks, CategoryViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(CategoryViewHolder categoryViewHolder, int i, LiveBooks liveBooks) {
+
+                Picasso.get().load(liveBooks.geturl()).into(categoryViewHolder.i1, new Callback() {
+                    @Override
+                    public void onSuccess() {
+
+                    }
+
+                    @Override
+                    public void onError(Exception e) {
+                        Toast.makeText(getContext(),"Image Load Failed",Toast.LENGTH_LONG).show();
+                    }
+                });
+                categoryViewHolder.t1.setText(liveBooks.getname());
+
+            }
+
+            @NonNull
+            @Override
+            public CategoryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_item,parent,false);
+
+                return new CategoryViewHolder(view);
+            }
+        };
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(
+                getContext(), LinearLayoutManager.HORIZONTAL,false
+        );
+        recyclerView.setLayoutManager(layoutManager);
+        adapter.startListening();
+        recyclerView.setAdapter(adapter);
+
+
+
+
+        databaseReference1 = FirebaseDatabase.getInstance().getReference().child("Upcoming_School");
+        options1 = new FirebaseRecyclerOptions.Builder<LiveBooks1>()
+                .setQuery(databaseReference1,LiveBooks1.class).build();
+        adapter1 = new FirebaseRecyclerAdapter<LiveBooks1, CategoryViewHolder1>(options1) {
+            @Override
+            protected void onBindViewHolder(CategoryViewHolder1 categoryViewHolder1, int i, LiveBooks1 liveBooks1) {
+
+                Picasso.get().load(liveBooks1.geturl()).into(categoryViewHolder1.i2, new Callback() {
+                    @Override
+                    public void onSuccess() {
+
+                    }
+
+                    @Override
+                    public void onError(Exception e) {
+                        Toast.makeText(getContext(),"Image Load Failed",Toast.LENGTH_LONG).show();
+                    }
+                });
+                categoryViewHolder1.t2.setText(liveBooks1.getname());
+
+            }
+
+            @NonNull
+            @Override
+            public CategoryViewHolder1 onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_itemm,parent,false);
+
+                return new CategoryViewHolder1(view);
+            }
+        };
+
+        LinearLayoutManager layoutManager2 = new LinearLayoutManager(
+                getContext(), LinearLayoutManager.HORIZONTAL,false
+        );
+        recyclerView1.setLayoutManager(layoutManager2);
+        adapter1.startListening();
+        recyclerView1.setAdapter(adapter1);
+
+
+
 
         return root;
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        if(adapter!=null)
+        {
+            adapter.startListening();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if(adapter!=null)
+        {
+            adapter.startListening();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if(adapter!=null)
+        {
+            adapter.startListening();
+        }
+    }
 
     public void flipperImages(int image)
     {
