dd770dc7cce6a2c2ea373e08b98ecc3091f14b84
njdunk07 <72277739+njdunk07@users.noreply.github.com>
2020-10-20 17:37:20 +0530

    Update ItemActivity.java

--- app/src/main/java/com/example/codemail/ItemActivity.java
+++ app/src/main/java/com/example/codemail/ItemActivity.java
@@ -83,7 +83,7 @@ public class ItemActivity extends AppCompatActivity {
                 view.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-
+                                                progressDialog.dismiss();
                     }
                 });
             }
